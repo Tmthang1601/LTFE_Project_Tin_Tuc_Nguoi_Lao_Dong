0288235d57bf7c3ca09c1ad764357262d4b500db
Tmthang1601 <21130537@st.hcmuaf.edu.vn>
2024-06-15 20:13:56 +0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import {  BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 const App = () => {
   return (
     <>
+    <Router>
       <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
+      
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand text-white" href="#">Navbar</a>
+            <Link class="navbar-brand text-white" to="/">
+                <h5>News Hub</h5>
+            </Link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
