83a447f8415956979e8d0ffff77f69758886f6dc
Tmthang1601 <21130537@st.hcmuaf.edu.vn>
2024-07-19 00:05:31 +0700

    Edit html content and edit css in Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 function Hero() {
     return (
         <div className={"container-fluid bg-dark text-white d-flex justify-content-center align-items-center flex-column"}
-                style={{height: "50vh"}}>
+             style={{height: "50vh"}}>
             <h1 style={{fontSize: "50px", color: "yellow"}}>BÁO TIN TỨC NGƯỜI LAO ĐỘNG</h1>
-            <h5>The Websites Build With The News API</h5>
+            <h5>Trang Web Này Được Xây Dựng Từ API Tin Tức Của nld.com.vn</h5>
         </div>
     );
 }
